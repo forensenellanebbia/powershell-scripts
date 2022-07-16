74efb20a47278dcf073289ae4f47fa7eda4940f6
Gabriele Zambelli <8271353+forensenellanebbia@users.noreply.github.com>
2022-07-16 19:47:11 +0200

    Update ConvertTo-LogonTracer.ps1

--- ConvertTo-LogonTracer.ps1
+++ ConvertTo-LogonTracer.ps1
@@ -11,47 +11,62 @@ GNU General Public License for more details.
 You can view the GNU General Public License at <http://www.gnu.org/licenses/>
 WARNING: This program is provided "as-is"
 *******************************************************************************
-
-Author      : Gabriele Zambelli @gazambelli
-Blog post   : https://forensenellanebbia.blogspot.com/2020/12/lets-combine-evtxecmd-with-logontracer.html
-
-This script:
-    - converts an XML created with EvtxECmd to an XML that can be imported into LogonTracer
-    - splits a large XML into smaller XML files
-    - can search for keywords
-
-Last updated: 2020-12-07
-Tested with : EvtxECmd (v0.6.0.3)  | https://ericzimmerman.github.io
-              LogonTracer (v1.5.0) | https://github.com/JPCERTCC/LogonTracer/releases/tag/v1.5.0
-How to use  : 
-    1) Parse the EVTX files with EvtxECmd and save the ouput in XML format.
-            Examples:
-                EvtxECmd.exe -d <path_to_your_evtx> --xml <output_path> --xmlf evtxecmd.xml --inc 4624,4625,4768,4769,4776,4672
-                EvtxECmd.exe -d <path_to_your_evtx> --xml <output_path> --xmlf evtxecmd.xml --inc 4624,4625,4768,4769,4776,4672 --sd "2020-12-06 00:00:00" --ed "2020-12-07 00:00:00"
-    2) Customize the settings of this script based on your needs by editing the lines in the Script Settings section below:
-        $SrcFile  = "<Absolute path to your XML source file>"
-        $DstPath  = "<Absolute path to your output folder>"
-        $SplitAt  = desired max number of events per XML ouput file
-        $Keywords = Default setting is an empty array which means no keywords to search for.
-            Examples:
-                @("") = default setting, no keywords
-                @("keyword1","keyword2","keyword3") = in the way shown, list one or more keywords to search for.
-                The search is case insensitive and the keywords are searched as substrings.
-    3) Run the PowerShell script: .\ConvertTo-LogonTracer.ps1
-       (Based on tests: ~320MB XML processed in ~16min)
-    4) Import each XML created by the script into LogonTracer
 #>
 
-# SCRIPT SETTINGS - BEGIN
-# Customize the script settings here
-$SrcFile  = "C:\TEMP\evtxecmd.xml"
-$DstPath  = "C:\TEMP"
-$SplitAt  = 5000
-$Keywords = @("")
-# SCRIPT SETTINGS - END
+<# 
+  .SYNOPSIS
+   This script converts an XML file created with EvtxECmd to an XML that can be imported into LogonTracer.
+  .DESCRIPTION
+   This script converts an XML file created with EvtxECmd to an XML that can be imported into LogonTracer. It can also split the input file into smaller XML files and can search for keywords. Based on tests: ~320MB XML processed in ~16min.
+
+   Test environment:
+   (OS) Windows 10 Pro ENG 21H2
+        # EvtxECmd (v1.0.0.0)
+        # Powershell 7.2.5 / Powershell 5.1
+   (OS) REMnux v7 Focal (Ubuntu 20.04.1 LTS)
+        # LogonTracer (Dockerfile: Dec 19, 2021) | https://github.com/JPCERTCC/LogonTracer/tree/master/docker
+  .PARAMETER InputXML
+   Path to the XML source file to parse. No other file formats are currently supported by the script.
+  .PARAMETER OutputPath
+   Output path
+  .PARAMETER SplitAt
+   Maximum number of events per XML ouput file
+  .PARAMETER Keywords
+   Comma separated keywords to search. The search is case insensitive and the keywords are searched as substrings.
+  .EXAMPLE
+   First parse the EVTX files with EvtxECmd and save the ouput in XML format. Examples:
+   EvtxECmd.exe -d <path_to_EVTX> --xml <output_path> --xmlf evtxecmd.xml --inc 4624,4625,4768,4769,4776,4672
+   EvtxECmd.exe -d <path_to_EVTX> --xml <output_path> --xmlf evtxecmd.xml --inc 4624,4625,4768,4769,4776,4672 --sd "2020-12-06 00:00:00" --ed "2020-12-07 00:00:00"
+   
+   Then run the script to make the XML file compatible with LogonTracer. Examples:
+
+   ConvertTo-LogonTracer.ps1 -InputXML .\evtxecmd.xml
+   ConvertTo-LogonTracer.ps1 -InputXML .\evtxecmd.xml -SplitAt 10000 -Keywords keyword1,keyword2,keyword3
+
+  .NOTES
+   Author       : Gabriele Zambelli
+   Twitter      : @gazambelli
+
+   CHANGELOG
+   2022-07-16: It's no longer needed to manually edit the script to set it up
+   2020-12-07: First release
+  .LINK
+   GitHub     : https://github.com/forensenellanebbia/powershell-scripts
+   Blog post  : https://forensenellanebbia.blogspot.com/2020/12/lets-combine-evtxecmd-with-logontracer.html
+#>
+
+[CmdletBinding()]
+Param (
+	[Parameter(Position=0,Mandatory = $True)][string]$InputXML,
+	[Parameter(Position=1,Mandatory = $False)][string]$OutputPath = (Get-ChildItem $InputXML).DirectoryName,
+	[Parameter(Position=2,Mandatory = $False)][int]$SplitAt = 5000,
+	[Parameter(Position=2,Mandatory = $False)][string[]]$Keywords = ""
+)
+
+$InputXML = (Get-ChildItem $InputXML).FullName
 
 function New-File ($DstPartNumber){
-    $DstFile = $DstPath + "\" + $DstFileTemplateName + "_" + $Suffix + "_" + $DstPartNumber + ".xml"
+    $DstFile = $OutputPath + "\" + $DstFileTemplateName + "_" + $Suffix + "_" + $DstPartNumber + ".xml"
     return $DstFile
    }
 
@@ -60,11 +75,12 @@ $StartTime = Get-Date -date $StartTimeNoFormat -format "yyyy-MM-dd HH:mm:ss"
 $Suffix    = Get-Date -date $StartTimeNoFormat -format "yyyyMMdd_HHmmss"
 $DstFileTemplateName = "EvtxECmd_LogonTracer"
 
+Write-Host "`n*** ConvertTo-LogonTracer (v2022-07-16) *** "
 Write-Host "Start Time         : $StartTime"
 Write-Host "`nINPUT"
-Write-Host "Source File        : $SrcFile"
+Write-Host "Source File        : $InputXML"
 
-$SrcNumberLines = [Linq.Enumerable]::Count([System.IO.File]::ReadLines($SrcFile))
+$SrcNumberLines = [Linq.Enumerable]::Count([System.IO.File]::ReadLines($InputXML))
 $SrcNumberLine  = 0
 $DstNumberLines = 1
 $DstPartNumber  = 1
@@ -75,10 +91,12 @@ Write-Host "Lines to parse     : $SrcNumberLines"
 Write-Host "`nOUTPUT"
 Write-Host "Keywords to search : $(($Keywords -notlike '').Count)" 
 Write-Host "Max events per file: $SplitAt"
-Write-Host "Output path        : $DstPath"
+Write-Host "Output path        : $OutputPath"
 Write-Host "Output file(s)     : $($DstFileTemplateName+'_'+$Suffix+'_###.xml')"
 
-foreach($SrcLine in [System.IO.File]::ReadLines($SrcFile)){
+$KeywordMatches = 0
+
+foreach($SrcLine in [System.IO.File]::ReadLines($InputXML)){
     $SrcNumberLine++
     $StepPercentage = [int]($SrcNumberLines/100)
     if($SrcNumberLine -eq 1){
@@ -100,6 +118,7 @@ foreach($SrcLine in [System.IO.File]::ReadLines($SrcFile)){
                 $Keywords | ForEach-Object{
                     if($MergedElements -match $_){
                         $KeywordFound++
+                        $KeywordMatches++
                     }
                 }
             }
@@ -142,4 +161,9 @@ foreach($SrcLine in [System.IO.File]::ReadLines($SrcFile)){
 
 Write-Host "Done!"
 $EndTime = Get-Date -format "yyyy-MM-dd HH:mm:ss"
+
+if(($Keywords -notlike '').Count -gt 0){
+	Write-Host "`nNumber of matches  : $KeywordMatches"
+}
+
 Write-Host "`nEnd Time: $EndTime"
